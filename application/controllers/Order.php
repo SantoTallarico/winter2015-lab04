34edb40b48498f16bb8c636ecf95da0917487369
Santo Tallarico <>
2015-02-05 17:06:00 -0800

    Sooooooooo I forgot to commit my changes throughout the tutorial, my bad, finished now.
    Some problems I found:
    
    Orders::total() has the line $items = $this->orderitems->some('code', $num);
    'code' should be 'order', as the orderitems table does not have a column 'code'
    
    You suggest some formatting functions for formatting the order total, money_format was one of them. However, money_format seems to be undefined in Windows. (I used number_format, hopefully that works across OS's)
    Signed-off-by:Santo Tallarico

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -17,19 +17,28 @@ class Order extends Application {
 
     // start a new order
     function neworder() {
-        //FIXME
+        $order_num = $this->orders->highest() + 1;
+        
+        $neworder = $this->orders->create();
+        $neworder->num = $order_num;
+        $neworder->date = date();
+        $neworder->status = 'a';
+        $neworder->total = 0;
+        $this->orders->add($neworder);
 
         redirect('/order/display_menu/' . $order_num);
     }
 
     // add to an order
     function display_menu($order_num = null) {
-        if ($order_num == null)
+        if ($order_num == null) {
             redirect('/order/neworder');
+        }
 
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        
+        $this->data['title'] = "Order # ".$order_num." (".number_format($this->orders->total($order_num), 2).")";
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -57,19 +66,19 @@ class Order extends Application {
 
     // inject order # into nested variable pair parameters
     function hokeyfix($varpair,$order) {
-	foreach($varpair as &$record)
+	foreach($varpair as &$record) {
 	    $record->order_num = $order;
+        }
     }
     
     // make a menu ordering column
     function make_column($category) {
-        //FIXME
-        return $items;
+        return $this->menu->some('category', $category);
     }
 
     // add an item to an order
     function add($order_num, $item) {
-        //FIXME
+        $this->orders->add_item($order_num, $item);
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -78,20 +87,39 @@ class Order extends Application {
         $this->data['title'] = 'Checking Out';
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        
+        $this->data['total'] = number_format($this->orders->total($order_num), 2);
+        
+        $items = $this->orderitems->group($order_num);
+        foreach ($items as $item)
+        {
+            $menuitem = $this->menu->get($item->item);
+            $item->code = $menuitem->name;
+        }
+        $this->data['items'] = $items;
 
         $this->render();
     }
 
     // proceed with checkout
-    function proceed($order_num) {
-        //FIXME
+    function commit($order_num) {
+        if (!$this->orders->validate($order_num)) {
+            redirect('/order/display_menu/' . $order_num);
+        }
+        $record = $this->orders->get($order_num);
+        $record->date = date(DATE_ATOM);
+        $record->status = 'c';
+        $record->total = $this->orders->total($order_num);
+        $this->orders->update($record);
         redirect('/');
     }
 
     // cancel the order
     function cancel($order_num) {
-        //FIXME
+        $this->orderitems->delete_some($order_num);
+        $record = $this->orders->get($order_num);
+        $record->status = 'x';
+        $this->orders->update($record);
         redirect('/');
     }
 
